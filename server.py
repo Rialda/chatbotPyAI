0c73c3f238b83c5f924ce4195447c2cb9a9896ac
rialda <rialda.spahic@gmail.com>
2018-04-22 22:21:22 +0200

    speech
    speech

--- server.py
+++ server.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
+
+#make Python speak
+import win32com.client as wincl
+speak = wincl.Dispatch("SAPI.SpVoice")
+
 app = Flask(__name__)
 
 english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
@@ -16,8 +21,10 @@ def home():
 
 @app.route("/get")
 def get_bot_response():
-    userText = request.args.get('msg')
-    return str(english_bot.get_response(userText))
+            userText = request.args.get(('msg'))
+            stringResponse=str((english_bot.get_response(userText)))
+            speechResponse= speak.Speak(str((english_bot.get_response(userText))))
+            return {'stringResponse':stringResponse, 'speechResponse':speechResponse}
 
 
 if __name__ == "__main__":
