96a16574faf9648c7d147b0acb195728306ad267
dzanabasic <dzana_basic@hotmail.com>
2018-05-07 18:04:30 +0200

    additional training done. Images and css changed
    additional training done. Images and css changed

--- server.py
+++ server.py
@@ -1,19 +1,28 @@
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
 
-
-#make Python speak
-import win32com.client as wincl
-speak = wincl.Dispatch("SAPI.SpVoice")
+import os
 
 app = Flask(__name__)
 
-english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter",
+            logic_adapters=[
+                    {
+                        "import_path": "chatterbot.logic.BestMatch",
+                        "statement_comparison_function": "chatterbot.comparisons.levenshtein_distance",
+                        "response_selection_method": "chatterbot.response_selection.get_first_response"
+                    }
+                ]
+                      )
 
 english_bot.set_trainer(ChatterBotCorpusTrainer)
 english_bot.train("chatterbot.corpus.english")
 
+conv=open('chats.txt', 'r').readlines()
+english_bot.set_trainer(ListTrainer) #set the trainer
+english_bot.train(conv) #train the bot
 
 @app.route("/")
 def home():
@@ -21,10 +30,8 @@ def home():
 
 @app.route("/get")
 def get_bot_response():
-            userText = request.args.get(('msg'))
-            stringResponse=str((english_bot.get_response(userText)))
-            speechResponse= speak.Speak(str((english_bot.get_response(userText))))
-            return {'stringResponse':stringResponse, 'speechResponse':speechResponse}
+    userText = request.args.get('msg')
+    return str(english_bot.get_response(userText))
 
 
 if __name__ == "__main__":
